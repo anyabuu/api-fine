6eab6170e35b15f5ce19be6b121e52132f5dc9c2
Anya Bu <annabukhtiiarova@gmail.com>
2023-01-04 18:21:59 +0200

    logic works;

--- client/index.js
+++ client/index.js
@@ -20,11 +20,11 @@ document.addEventListener('DOMContentLoaded', async () => {
   const adminPage = document.querySelector('.admin');
   const fineForm = document.querySelector('.admin__fine-form');
 
-  const profilePage = document.querySelector('.user__profile');
   const changeEmailForm = document.querySelector('.user__change-email-form');
   const changePasswordForm = document.querySelector('.user__change-password-form');
 
   const logoutButton = document.querySelector('.header__nav-link-logout');
+
 
   function lengthValidate(element, minLength, maxLength) {
     if (element.value.length < minLength || element.value.length > maxLength) {
@@ -134,23 +134,16 @@ document.addEventListener('DOMContentLoaded', async () => {
             return response.json();
           })
           .then((data) => {
-
-            console.log(data)
-
             if (data.token) {
               storage.token = data.token;
               storage.email = data.email;
-              console.log(storage.token)
               sessionStorage.userinfo = JSON.stringify(storage);
-
 
               if (data.email === 'admin@rammfall.com'){
                 location.href = 'http://localhost:4002/admin.html';
                 return
               }
-
               location.href = 'http://localhost:4002/user.html';
-
               return
             }
             loginErrorMessage.innerText = data.message;
@@ -164,7 +157,6 @@ document.addEventListener('DOMContentLoaded', async () => {
   }
 
   const userInfoObj = JSON.parse(sessionStorage.getItem('userinfo'))
-
 
   if (userPage) {
 
@@ -187,8 +179,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     const userTitle = document.querySelector('.user__title-name');
     userTitle.innerText = `${storage.email}`;
     const balanceForm = document.querySelector('.user__balance-form');
-
-
 
      fetch(`${HOST}/balance`, {
       method: "GET",
@@ -246,10 +236,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         .catch((err) => {
           console.log(err)
         })
-
-
     })
-
 
 
     changeEmailForm.addEventListener('submit', async (event) => {
@@ -306,7 +293,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         body,
         method: "PATCH",
         headers: { token: userInfoObj.token }
-
       })
         .then((response) => {
           return Promise.all([response.json(), response.ok]);
@@ -340,26 +326,134 @@ document.addEventListener('DOMContentLoaded', async () => {
       .then((data) => {
 
         console.log(data)
-        // const list = document.querySelector('.user__fines-list');
-        //
-        // // list.innerHTML=''
-        //
-        //
-        // data.forEach(function({ id, description, deadline, date, amount, paid }){
-        //   const listItem = document.createElement('li');
-        //   listItem.classList.add('user__fines-item');
-        //   const itemDescription = document.createElement('div')
-        //   itemDescription.classList.add('user__fines-description')
-        //   itemDescription.innerText = `${description}`;
-        //   itemDescription.append(listItem);
-        //   list.append(listItem);
-        // })
+        const list = document.querySelector('.user__fines-list');
+
+        list.innerHTML = ''
+
+        data.forEach(function({ id, description, deadline, amount, paid }){
+          const listItem = document.createElement('li');
+          listItem.classList.add('user__fines-item');
+          listItem.setAttribute('value', `${id}`);
+
+          const date = new Date(Date.parse(deadline));
+          const year = date.getFullYear();
+          const month = date.getMonth() + 1;
+          const day = date.getDay();
+          const yearDate = [
+            day.toString().padStart(2, '0'),
+            month.toString().padStart(2, '0'),
+            year,
+          ].join('-')
+
+          let paidStatus = 'unpaid'
+
+          if (paid === true) {
+            paidStatus = 'paid'
+            listItem.classList.add('user__fines-item-paid')
+          }
+
+          listItem.innerHTML =
+            `
+            <div class='user__fines-description user__fines-cell'>
+              ${description}
+            </div>
+             <div class='user__fines-amount user__fines-cell' data-value='${amount}'>
+              ${amount}$
+            </div>
+            <div class='user__fines-deadline user__fines-cell'>
+              ${yearDate}
+            </div>
+            <div class='user__fines-status user__fines-cell' data-value='${paid}'>
+              ${paidStatus}
+            </div>
+            <button class='user__fines-pay-button button'>
+              Pay
+            </button>
+            `
+
+          if (paid === true) {
+            const button = document.querySelector('.user__fines-pay-button');
+            button.classList.add('disable')
+          }
+
+
+          list.append(listItem);
+        })
+
+
+        //PAY FINE
+
+        list.addEventListener('click', function(event) {
+          const target = event.target;
+
+          if (event.target.matches('button')) {
+
+            const targetItem = target.closest('li');
+            const amount = targetItem.querySelector('.user__fines-amount');
+            const amountValue = amount.dataset.value;
+            const paidStatus = targetItem.querySelector('.user__fines-status');
+            let paidStatusValue = paidStatus.dataset.value;
+
+
+            fetch(`${HOST}/pay/fine/${targetItem.value}`, {
+              method: 'PATCH',
+              headers: { token: userInfoObj.token },
+
+            })
+              .then((response) => {
+                console.log(response);
+                return response.json();
+              })
+              .then((dat) => {
+                console.log(dat);
+
+
+                fetch(`${HOST}/balance`, {
+                  method: "GET",
+                  headers: { token: userInfoObj.token }
+                })
+                  .then((response) => {
+                    return response.json();
+                  })
+                  .then((data) => {
+                    console.log(data)
+
+                    if (data.balance < amountValue) {
+                      return
+                    }
+
+                    console.log('errrrrrrrr')
+
+                    balance.innerText = `Balance: ${data.balance - amountValue}$`
+                    targetItem.classList.add('user__fines-item-paid');
+                    event.target.classList.add('disable')
+
+                    paidStatus.dataset.value = 'true';
+                    paidStatus.innerText = 'paid';
+
+                  })
+                  .catch((err) => {
+                    console.log(err)
+                  })
+
+                alert(dat.message);
+
+
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+
+
+          }
+
+        });
+
+
       })
       .catch((err) => {
         console.log(err)
       })
-
-
 
 
   }
@@ -368,34 +462,25 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   if (adminPage){
 
-    const getUserButton = document.querySelector('.admin__get-user-button');
-
-    getUserButton.onclick = () => {
-
-      fetch(`${HOST}/users`, {
-        method: "GET",
-        headers: { token: userInfoObj.token }
-      })
-        .then((response) => {
-          return response.json();
+    fetch(`${HOST}/users`, {
+          method: "GET",
+          headers: { token: userInfoObj.token }
         })
-        .then((data) => {
-          const list = document.querySelector('.admin__user-list');
-
-          list.innerHTML=''
-
-          data.forEach(function({ id, email, name }){
-            const listItem = document.createElement('li');
-            listItem.innerText = `${id} ${name} ${email}`;
-            list.append(listItem);
+          .then((response) => {
+            return response.json();
           })
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    }
-
-
+          .then((data) => {
+            const select = document.querySelector('.admin__form-select');
+            select.innerHTML=''
+            data.forEach(function({ id, email, name }){
+              const text = `${id} - ${name} - ${email}`
+              const newOption = new Option(text, id);
+              select.add(newOption);
+            })
+          })
+          .catch((err) => {
+            console.log(err)
+          })
 
 
     fineForm.addEventListener('submit', async (event) => {
